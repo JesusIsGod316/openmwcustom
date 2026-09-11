5502832d239298dbc7efa8f03f22466fdf8e3f36
Chance <l.s.chance2000@gmail.com>
2026-09-11 10:33:29 -0500

    CP4F: compile native local-map gameplay bridge

--- apps/openmw/mwrender/v4engine-sources.cmake
+++ apps/openmw/mwrender/v4engine-sources.cmake
@@ -3,6 +3,7 @@
 set(OPENMW_V4_ENGINE_RUNTIME_SOURCES
     "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4engineframecoordinator.cpp"
     "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4enginerenderbridge.cpp"
+    "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4localmapbridge.cpp"
     "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4runtimeoptions.cpp"
     "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4scenerenderlifecycle.cpp"
     "${CMAKE_SOURCE_DIR}/apps/openmw/mwrender/v4semanticsource.cpp"
