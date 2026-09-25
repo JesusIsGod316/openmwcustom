be4af703c764a79d8805fc7fa22eb5fc4f96b31d
Chance <l.s.chance2000@gmail.com>
2026-09-25 17:54:42 -0500

    OptimizedMW GL-P4R: add class-aware compile cost model

--- components/resource/openmwcompileoperation.hpp
+++ components/resource/openmwcompileoperation.hpp
@@ -24,6 +24,8 @@ namespace Resource
         double mDeleteBudgetMs = 0.15;
         unsigned int mMaxObjectsPerFrame = 4;
         double mDiagnosticThresholdMs = 0.20;
+        double mMinimumDrainBudgetMs = 0.35;
+        double mHeavyOpThresholdMs = 4.0;
     };
 
     class OpenMWIncrementalCompileOperation final : public osgUtil::IncrementalCompileOperation
@@ -52,8 +54,17 @@ namespace Resource
         struct CostState
         {
             double mEmaMs = 0.0;
-            double mMaxMs = 0.0;
+            double mHighWaterMs = 0.0;
+            double mEstimateScale = 1.0;
             std::uint64_t mSamples = 0;
+        };
+
+        struct Prediction
+        {
+            double mPredictedMs = 0.0;
+            double mOsgEstimateMs = 0.0;
+            double mEmaMs = 0.0;
+            double mEstimateScale = 1.0;
         };
 
         struct SeenState
@@ -62,17 +73,20 @@ namespace Resource
         };
 
         CompileKind classify(const CompileOp* op) const;
-        double predictedMs(CompileOp* op, CompileInfo& info, CompileKind kind) const;
-        void observe(CompileKind kind, double actualMs);
+        Prediction predictedCost(CompileOp* op, CompileInfo& info, CompileKind kind, const CompileSet* set) const;
+        void observe(CompileKind kind, const CompileSet* set, double osgEstimateMs, double actualMs);
         void finishCompileSet(CompileSet* set);
         void pruneSeen(const CompileSets& queued);
         static int compileClassRank(const CompileSet* set);
+        static std::size_t compileClassIndex(const CompileSet* set);
         static const char* compileClassName(const CompileSet* set);
         static const char* compileKindName(CompileKind kind);
 
         OpenMWCompileSchedulerConfig mConfig;
         P4CompilePolicyState mPolicyState;
-        std::array<CostState, static_cast<std::size_t>(CompileKind::Count)> mCosts{};
+        static constexpr std::size_t sCompileClassCount = 4;
+        static constexpr std::size_t sCompileKindCount = static_cast<std::size_t>(CompileKind::Count);
+        std::array<std::array<CostState, sCompileKindCount>, sCompileClassCount> mCosts{};
         std::unordered_map<const CompileSet*, SeenState> mSeen;
         std::size_t mLastQueueDepth = 0;
 
