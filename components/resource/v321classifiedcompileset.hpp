d95515fd3a65d14fc92524297a9e5b6d28b9a7ed
Chance <l.s.chance2000@gmail.com>
2026-09-26 10:38:12 -0500

    OptimizedMW P6: add compile urgency metadata

--- components/resource/v321classifiedcompileset.hpp
+++ components/resource/v321classifiedcompileset.hpp
@@ -16,6 +16,15 @@ namespace Resource
         GenericModel,
     };
 
+    // P6 residency/deadline metadata. This describes producer intent rather
+    // than current visibility.
+    enum class V321CompileUrgency : unsigned char
+    {
+        Background = 0,
+        NearFuture,
+        Required,
+    };
+
     inline std::atomic_bool& v321CP2FairnessFlag()
     {
         static std::atomic_bool enabled{ false };
@@ -36,19 +45,23 @@ namespace Resource
     class V321ClassifiedCompileSet final : public osgUtil::IncrementalCompileOperation::CompileSet
     {
     public:
-        V321ClassifiedCompileSet(osg::Node* subgraph, V321CompileClass compileClass)
+        V321ClassifiedCompileSet(osg::Node* subgraph, V321CompileClass compileClass,
+            V321CompileUrgency urgency = V321CompileUrgency::NearFuture)
             : CompileSet(subgraph)
             , mCompileClass(compileClass)
+            , mUrgency(urgency)
         {
         }
 
         V321CompileClass compileClass() const { return mCompileClass; }
+        V321CompileUrgency urgency() const { return mUrgency; }
 
     protected:
         ~V321ClassifiedCompileSet() override = default;
 
     private:
         V321CompileClass mCompileClass;
+        V321CompileUrgency mUrgency;
     };
 
     inline V321CompileClass getV321CompileClass(
@@ -56,5 +69,12 @@ namespace Resource
     {
         const auto* classified = dynamic_cast<const V321ClassifiedCompileSet*>(compileSet);
         return classified ? classified->compileClass() : V321CompileClass::Unknown;
+    }
+
+    inline V321CompileUrgency getV321CompileUrgency(
+        const osgUtil::IncrementalCompileOperation::CompileSet* compileSet)
+    {
+        const auto* classified = dynamic_cast<const V321ClassifiedCompileSet*>(compileSet);
+        return classified ? classified->urgency() : V321CompileUrgency::NearFuture;
     }
 }
